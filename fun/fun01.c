19eddbe52484266322042689dad906e76c78d1c7
liu <2992354230@qq.com>
2026-03-08 22:59:33 +0800

    修改fun fun01.c

--- fun/fun01.c
+++ fun/fun01.c
@@ -4,4 +4,16 @@
 void fun01(int n) {
     printf("fun01");
     return;
+
+}
+void fun02(int n) {
+    
+    printf("fun02");
+    return;
+}
+
+void fun03(int n) {
+    
+    printf("fun03");
+    return;
 }
